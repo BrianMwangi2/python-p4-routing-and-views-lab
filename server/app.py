8b8b920010f19c0d6d74d3bb3bcaf4232085785c
Brian <brian.mwangi2@student.moringaschool.com>
2024-06-27 07:38:24 +0300

    passes

--- server/app.py
+++ server/app.py
@@ -1,8 +1,42 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_route(parameter):
+    print(parameter)
+    return parameter
+
+
+@app.route('/count/<int:parameter>')
+def count_route(parameter):
+    count = ''
+    for i in range(parameter):
+        count += str(i) + '\n'
+    return count
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math_route(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        if num2!= 0:
+            return str(num1 / num2)
+        else:
+            return 'Error: Division by zero'
+    elif operation == '%':
+        if num2!= 0:
+            return str(num1 % num2)
+        else:
+            return 'Error: Division by zero'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run()
